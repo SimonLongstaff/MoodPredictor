963eb6038092df3b50e142766f34c9b01474b8a9
sxl1173 <sxl1173@cs.bham.ac.uk>
2020-07-10 22:56:29 +0100

    Create pin on map, set location name, click to print location information

--- app/src/main/java/com/example/moodpredictor/MapFragment.java
+++ app/src/main/java/com/example/moodpredictor/MapFragment.java
@@ -1,17 +1,24 @@
 package com.example.moodpredictor;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import androidx.annotation.Nullable;
+import java.util.Objects;
+
 import androidx.fragment.app.Fragment;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
@@ -20,29 +27,49 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     GoogleMap mGoogleMap;
     MapView mMapView;
     View mView;
+    double lattiude;
+    double longitude;
+    String title;
 
+    //Constructor
+    public MapFragment() {
 
-    public MapFragment(){
 
     }
 
+    //Set up
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        mView = inflater.inflate(R.layout.map_fragment,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        mView = inflater.inflate(R.layout.map_fragment, container, false);
         return mView;
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState){
-        super.onViewCreated(view,savedInstanceState);
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final EditText enterTitle = (EditText) Objects.requireNonNull(getView().findViewById(R.id.addTitle));
+
+        ImageButton add =  Objects.requireNonNull(getView()).findViewById(R.id.addNewPlace);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                title = enterTitle.getText().toString();
+                createSavedLocation();
+            }
+        });
+
+
 
         mMapView = (MapView) mView.findViewById(R.id.map);
-        if (mMapView != null){
+        if (mMapView != null) {
             mMapView.onCreate(null);
             mMapView.onResume();
             mMapView.getMapAsync(this);
@@ -55,9 +82,27 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
 
         MapsInitializer.initialize(getContext());
-
         mGoogleMap = googleMap;
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mGoogleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mGoogleMap.clear();
+                MarkerOptions marker = new MarkerOptions()
+                        .position(new LatLng(latLng.latitude, latLng.longitude))
+                        .title("New Marker");
+                mGoogleMap.addMarker(marker);
+                lattiude = latLng.latitude;
+                longitude = latLng.longitude;
+                System.out.println(lattiude + "---" + longitude);
+            }
+        });
 
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
+
+    public void createSavedLocation(){
+        System.out.println("Title: " + title + "\nLongitude: " + longitude + "\n  Latitude: " + lattiude);
+    }
+
+
 }
